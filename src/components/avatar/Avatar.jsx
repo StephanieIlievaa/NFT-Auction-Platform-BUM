9a74c293548c3c2928a69e3b8a8c938e2ef01fff
Stephanie <stephanieilieva1995@gmail.com>
2021-10-31 17:12:30 +0200

    removed react container component and changed it

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,16 +1,16 @@
-import Container from '@mui/material/Container'
 import styles from "./Avatar.module.scss"
 
-function Avatar({ size=90, url, verified = false}) {
+function Avatar({ size = 90, url, verified = false}) {
 return (
 <div className={styles.wrapper}>
-    <Container className={styles.avatar} sx={{width: size, height: size}}>
+    <div className={styles.avatar} sx={{width: size, height: size}}>
 <img className={styles.image} src={url}></img>
 { verified 
-? <img className={styles.badge} src={'../../../public/images/verified.svg'} /> 
+? <img className={styles.badge} src={'../../../public/images/verified.svg'}/> 
 : null}
-    </Container>
+    </div>
      </div>
 );
 }
+
 export default Avatar;
