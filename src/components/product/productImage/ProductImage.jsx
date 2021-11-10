4dcb8354b8bc3d40de8a264fbdfa4ce5da36c0ae
Stephanie <stephanieilieva1995@gmail.com>
2021-11-10 17:12:36 +0200

    Update

--- src/components/product/productImage/ProductImage.jsx
+++ src/components/product/productImage/ProductImage.jsx
@@ -1,7 +1,7 @@
 import styles from "./ProductImage.module.scss";
 import Paper from "@mui/material/Paper";
 
-export default function ProductImage({url =[]}) {
+export default function ProductImage({url=[]}) {
   return (
     <div className={styles.product-image}>
       <Paper>
--- src/components/product/productInfoTitle/ProductInfoTitle.jsx
+++ src/components/product/productInfoTitle/ProductInfoTitle.jsx
@@ -1,12 +1,12 @@
-import styles from './ProductInfoTitle.module.scss';
-import { Typography } from '@mui/material';
+import styles from "./ProductInfoTitle.module.scss";
+import { Typography } from "@mui/material";
 
-export default function ProductInfoTitle() {
-    return ( 
-        <div className={styles.product-info-title}> 
-<Typography className={styles.title}>
-
-</Typography>
-        </div>
-    )
+export default function ProductInfoTitle(title = []) {
+  return (
+    <div className={styles.product - info - title}>
+      <Typography className={styles.title} variant="h1">
+        {title}
+      </Typography>
+    </div>
+  );
 }
