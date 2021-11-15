dbfb97113a0c7c17c55680c109476a48e37cb992
Stephanie <stephanieilieva1995@gmail.com>
2021-11-15 11:04:29 +0200

    Update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,18 +8,18 @@ export default function Footer() {
   return (
     <div className={styles.wrapper}>
       <Container className={styles.footerContainer} maxWidth="xl">
-        <Grid container direction="row">
-          <Grid item>
-            <Logo type="muted" className={styles.logo} />
+        <Grid container >
+          <Grid item sx={6}sx={{display: "flex-start", paddingTop: "20px"}}>
+            <Logo type="muted"  />
           </Grid>
           <Grid
-            item xs
+            item xs={6}
             className={styles.allRights}
-            sx={{ display: "flex", justifyContent: "center" }}
+            sx={{ display: "flex", paddingLeft: '500px', paddingTop: "25px" }}
           >
             <p>Bum All Rights Reserved 2021</p>
           </Grid>
-          <Grid item xs sx={{ display: "flex", justifyContent: "flex-end", paddingTop: "25px" }}>
+          <Grid item xs sx={{ display: "flex", justifyContent: "flex-end", paddingTop: "15px" }}>
             <Button sx={{ color: "#e1e1fc" }}>Privacy Policy</Button>
             <Button sx={{ color: "#e1e1fc" }}>Cookie Policy</Button>
           </Grid>
