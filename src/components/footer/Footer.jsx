73037e4dbfb54313cc8dfb6da7e18fecedf9a98d
Stephanie <stephanieilieva1995@gmail.com>
2021-11-15 10:42:55 +0200

    Update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,23 +2,24 @@ import Grid from "@mui/material/Grid";
 import Logo from "../logo/Logo.jsx";
 import Button from "@mui/material/Button";
 import styles from "./Footer.module.scss";
-import { Container } from "@mui/material";
+import Container from "@mui/material/Container";
 
 export default function Footer() {
   return (
     <div className={styles.wrapper}>
       <Container className={styles.footerContainer} maxWidth="xl">
-        <Grid container  direction="row" >
-          <Grid item >
-            <Logo type="muted" sx={{ zIndex: 'modal' }} />
+        <Grid container direction="row">
+          <Grid item>
+            <Logo type="muted" className={styles.logo} />
           </Grid>
           <Grid
+            item xs
             className={styles.allRights}
             sx={{ display: "flex", justifyContent: "center" }}
           >
             <p>Bum All Rights Reserved 2021</p>
           </Grid>
-          <Grid item xs sx={{ direction : 'row', display: 'flex-end' }} >
+          <Grid item xs sx={{ display: "flex", justifyContent: "flex-end", paddingTop: "25px" }}>
             <Button sx={{ color: "#e1e1fc" }}>Privacy Policy</Button>
             <Button sx={{ color: "#e1e1fc" }}>Cookie Policy</Button>
           </Grid>
@@ -27,5 +28,3 @@ export default function Footer() {
     </div>
   );
 }
-
-
