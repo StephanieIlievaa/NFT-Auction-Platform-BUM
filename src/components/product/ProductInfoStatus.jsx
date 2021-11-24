3599a330dd2be4ddd156195ea37bd28a31120375
Stephanie <stephanieilieva1995@gmail.com>
2021-11-24 10:36:31 +0200

    update class in ProductInfoStatus component

--- src/components/product/ProductInfoStatus.jsx
+++ src/components/product/ProductInfoStatus.jsx
@@ -2,9 +2,12 @@ import styles from "./ProductInfoStatus.module.scss";
 import { Chip } from "@mui/material";
 
 export default function ProductInfoStatus({ isLive }) {
-  let liveStatus = 
-    <Chip className={styles.status} label="LIVE" color="success" />
-  
+  let liveStatus = (
+    <div>
+      <Chip className={styles.status} label="LIVE" color="success" />
+    </div>
+  );
+
   return (
     <div className={`${styles["product-info-status"]}`}>
       {isLive ? liveStatus : <div></div>}
