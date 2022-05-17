7b2f275cebc9820dbf655d7c5eb3b9e22b31b299
Stefani Ilieva <stephanieilieva1995@gmail.com>
2022-05-17 23:51:05 +0300

    feat: add framer-motion to Featured component

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -3,33 +3,40 @@ import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import Container from "@mui/material/Container";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import { AnimatePresence, AnimateSharedLayout, motion } from "framer-motion";
 
 export default function Featured({ items = [] }) {
-
+  const [selectedId, setSelectedId] = useState(null);
   return (
-  
     <div>
       <Container className={styles.container}>
-        <ImageList className={styles.imageList} cols={6}>
-
-          {items.map((item, index) => (
-            <ImageListItem
-              className={styles.imageListItem}
-              key={item.id}
-              cols={index === 0 ? 3 : 1}
-              rows={index === 0 ? 2 : 1}
-            >
-              <img
+        |{" "}
+        <AnimateSharedLayout type="crossfade">
+          
+            {items.map((item, index) => (
+              // <motion.img
+              //   className={styles.imageListItem}
+              //   key={item.id}
+              //   cols={index === 0 ? 3 : 1}
+              //   rows={index === 0 ? 2 : 1}
+              // >
+              <motion.img
                 className={styles.image}
                 src={item.source.url}
                 srcSet={item.title}
                 alt={item.title}
-                //product + item.id 
+                //product + item.id
                 onClick={() => useRouter().push(item.href)}
               />
-            </ImageListItem>
-          ))}
-        </ImageList>
+              // </ImageListItem>
+            ))}
+         <AnimatePresence>
+  {productIds.map(id => (
+    <ProductImage id={id} key={id} onExpand={setAsPrimary} />
+  ))}
+</AnimatePresence>
+        </AnimateSharedLayout>
       </Container>
     </div>
   );
